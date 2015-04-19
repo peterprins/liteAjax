89b056e3e646573fba407f75188d99eebca7c6e9
Peter Prins <peter@dotnoise.com>
2015-04-19 12:14:51 -0700

    liteAjax v1.0
    lieAjax version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # liteAjax
 Lite Ajax
+
+2k minified Ajax Javascript Library and Bonus Class for Modern Browsers to load JSON and/or any other file with AJAX no other library needed.
+
+LIBRARY SYNTAX
+
+This command loads 'test.json' into an html element with the id of 'test' and does so after the document has loaded and is ready...
+
+$ajax('test.json', 'test').load();
+
+You can also load text just as easy...
+
+$ajax('test.txt', 'test').load();
+
+You can specify the properties... (but you don't have to)
+
+$ajax( { url: 'test.json',
+		method: 'GET',
+		type: 'application/json' }).load();
+
+You can specify a callback function if you want...
+
+function mycallback(data){
+	console.log('data: ', data);
+	document.writeln('data: ' + data);
+}
+
+$ajax( { url: 'test.json',
+		method: 'GET',
+		type: 'application/json' }, mycallback).load();
+
+
+Or you can use an anonymous function...
+
+$ajax( { url: 'test.txt',
+		type: 'text/plain' }, function(data){
+
+			console.log('data: ', data);
+			$ajax().updateElement('test', data);
+
+}).load();
+
+And of course you can send POST data and it will set the type for you...
+
+$ajax( { url: 'test.json',
+		method: 'POST',
+		data: '{ "test": { "testing": "1 2 3" } }' }, function(data){
+
+			console.log('data: ', data);
+			$ajax().updateElement('test', data);
+
+}).load();
+
+CLASS SYNTAX
+
+load 'test.json' into 'test' id
+
+var test = new liteAjax('test.json', 'test');
+test.load();
+
+load 'test.json' and run mycallback
+
+function mycallback(){
+	console.log('data: ', data);
+	test.updateElement('test', data);
+}
+
+var test = new liteAjax( {
+	url: 'test.json',
+	method: 'GET',
+	type: 'application/json' }, mycallback);
+
---
+++ liteAjax.class.js
@@ -0,0 +1,182 @@
+/*!
+  * liteAjax - v1.0 
+  * https://github.com/peterprins/liteajax
+  *
+  * Copyright 2015 Peter Prins
+  * Released under the MIT license
+  * http://mit-license.org/
+  *
+  * Date: 2015-04-19
+  */
+
+// this is the class
+function liteAjax(request_obj,callback_fn){
+
+	this.url;
+	this.method;
+	this.type;
+	this.data;
+	this.callback;
+	this.response;
+	this.element_id;
+	this.json;
+
+	// update element
+	this.updateElement = function(id,data){
+
+		//console.log('updateElement');
+
+		// check typeof
+		if((typeof data == 'string' || typeof data == 'object')
+			&& typeof id == 'string'){
+			var element = document.getElementById(id);
+
+			try{
+				element.innerHTML = JSON.stringify(data);
+			}catch(e){
+				element.innerHTML = data;
+			}
+
+		}else{
+
+			// check status
+			if(this.status == 200){
+				var element = document.getElementById(element_id);
+				element.innerHTML = this.response;
+			}
+
+		}
+		
+	}
+
+	// get json obj
+	this.getJSONObj = function(){
+
+		//console.log('getJSONObj');
+
+		// check status
+		if(this.status == 200){
+			try{
+				var json_obj = JSON.parse(this.response);
+			}catch(e){
+				var json_obj = this.response;
+			}
+		}
+
+		// do callback fn
+		callback_fn(json_obj);
+
+	}
+
+	// init
+	this.init = function(){
+
+		//console.log('init');
+
+		// get url
+		if(typeof request_obj !== 'object' && typeof request_obj !== 'string'){
+
+			// init props
+			this.method = 'GET';
+			this.type = 'application/json';
+			this.callback = null;
+
+		}else{
+
+			// get url
+			if(typeof request_obj == 'object'){
+				this.url = request_obj['url'];
+			}else if(typeof request_obj == 'string'){
+				this.url = request_obj;
+			}
+	  
+			// get method
+			this.method = request_obj['method'] ? request_obj['method'] : 'GET';
+	  
+	  		// get type
+			if( this.method == 'POST' ){
+				this.type = request_obj['type'] ? request_obj['type'] : 'application/x-www-form-urlencoded;charset=UTF-8';
+				
+			}else{
+				this.type = request_obj['type'] ? request_obj['type'] : 'application/json';
+			}
+
+			// get data
+			this.data = request_obj['data'] ? JSON.stringify(request_obj['data']) : null;
+	 
+		}
+	}
+
+	// load ajax
+	this.load = function(){
+
+		//console.log('load');
+
+		//init
+		this.init();
+	
+		// create object
+		json = new XMLHttpRequest();
+
+		// set content type 
+		json.type = this.type;
+
+		// is doc loaded ?
+		document.addEventListener( 'DOMContentLoaded', this.send(json), false);
+
+	}
+
+	// send ajax
+	this.send = function(json){
+
+		//console.log('send');
+
+		// get callback
+		if(typeof callback_fn == 'function'){
+
+			this.callback = this.getJSONObj;
+
+		}else if(typeof callback_fn == 'string'){
+
+			this.element_id = callback_fn;
+			element_id = callback_fn;
+
+			this.callback = this.updateElement;
+
+		}else{
+
+			this.callback = null;
+		}
+
+		// do we have a callback
+		if(this.callback !== null){
+
+			// modern browsers
+			if( json.addEventListener ) {
+
+				// when file is loaded do callback
+				json.addEventListener( 'load', this.callback, false);
+
+			}else if( json.readyState ){
+
+				// microsoft IE9 compatability
+				// if(json.readyState == 4) success
+				json.onreadystatechange = this.callback;
+			}
+
+		}
+
+		// open file
+		json.open( this.method , this.url );
+
+		// if POST
+		if( this.method == 'POST'){
+			json.setRequestHeader( 'Content-type', this.type );
+		}
+
+		// send request
+		json.send( this.data );
+
+	}
+
+}
